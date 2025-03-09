301b5d8bfc87c7bc4ea377d48359006f86bce881
Hoshiyoru <leejm-wm24@student.tarc.edu.my>
2025-03-09 20:21:34 +0800

    Update main.java

--- assignmentLJM/src/main/java/main.java
+++ assignmentLJM/src/main/java/main.java
@@ -1,10 +1,10 @@
-//090325Version
-//Need to connect sql, please whatsapp me to get details.
+//090325. Version
+//Need to connect SQL, please WhatsApp me to get details.
 //Or you can search about how to import SQLite in Java
 //Please download SQLite extension
 
-//My program got three role call: guest, admin and staff
-//These role got different permission
+//My program got three role calls: guest, admin, and staff
+//These roles got different permission
 //This version guest and staff only can login
 //Admin can register staff
 
@@ -12,8 +12,14 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class main {
-    //mydatabase.db can change to your database name(but must end with .db lah)
+    //mydatabase.db can change to your database name (but must end with .db lah)
     static final String url = "jdbc:sqlite:mydatabase.db";
+    public static final String ANSI_RED = "\u001B[31m";;
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_RESET = "\u001B[0m";
 
     //Ok everybody know this is main
     public static void main(String[] args) {
@@ -26,40 +32,40 @@ public class main {
         System.out.println("Staff Page");
 
         while (true) {
-            System.out.println("[1] Login");
-            System.out.println("[2] Logout");
-            System.out.println("[3] Exit");
-            System.out.print("Enter your choice: ");
+            System.out.println(ANSI_GREEN + "[1] Login" + ANSI_RESET);
+            System.out.println(ANSI_YELLOW + "[2] Logout" + ANSI_RESET);
+            System.out.println(ANSI_RED + "[0] Exit" + ANSI_RESET);
+            System.out.print(ANSI_BLUE + "Enter your choice: " + ANSI_RESET);
 
             int staffPageChoice;
             if (sc.hasNextInt()) {
                 staffPageChoice = sc.nextInt();
             } else {
-                System.out.println("Invalid input! Please enter a number.");
+                System.out.println(ANSI_RED + "Invalid input! Please enter a number." + ANSI_RESET);
                 sc.next();
                 continue;
             }
 
             switch (staffPageChoice) {
                 case 1:
-                    role = userCheck.login(sc);
+                    role = UserCheck.login(sc);
                     break;
                 case 2:
-                    System.out.println("Logout successful!");
+                    System.out.println(ANSI_PURPLE + "Logout successful!" + ANSI_RESET);
                     role = "guest";
                     break;
-                case 3:
-                    System.out.println("Exiting...");
+                case 0:
+                    System.out.println(ANSI_YELLOW + "Exiting..." + ANSI_RESET);
                     sc.close();
                     return;
                 default:
-                    System.out.println("Invalid choice. Please try again.");
+                    System.out.println(ANSI_RED + "Invalid choice. Please try again." + ANSI_RESET);
             }
         }
     }
 
     public static void database() {
-        //I create two table for different types of role but usually same lah
+        //I create two tables for different types of roles but usually the same lah
         String createAdminTable = "CREATE TABLE IF NOT EXISTS admin ("
                 + "username VARCHAR(50) PRIMARY KEY,"
                 + "password VARCHAR(50) NOT NULL)";
@@ -82,16 +88,16 @@ public class main {
                     //Not safe because everybody know admin acc
                     String insertAdmin = "INSERT INTO admin (username, password) VALUES ('admin', '12345')";
                     stmt.execute(insertAdmin);
-                    System.out.println("Default admin account created: admin / 12345");
+                    System.out.println(ANSI_RED + "Default admin account created: admin / 12345" + ANSI_RESET);
                 }
             }
         } catch (SQLException e) {
-            System.out.println("Database Error: " + e.getMessage());
+            System.out.println(ANSI_RED + "Database Error: " + e.getMessage() + ANSI_RESET);
         }
     }
 }
 
-//Check the username user want to register exists or not
+//Check the username user wants to register exists or not
 //If the people got same name I dunno
 class UserCheck {
     public void checkUser(String username) {
@@ -104,9 +110,9 @@ class UserCheck {
 
             try (ResultSet rs = stmt.executeQuery()) {
                 if (rs.next()) {
-                    System.out.println("Username already exists!");
+                    System.out.println(main.ANSI_YELLOW + "Username already exists!" + main.ANSI_RESET);
                 } else {
-                    System.out.println("Username is available.");
+                    System.out.println(main.ANSI_RED + "Username is available." + main.ANSI_RESET);
                 }
             }
         } catch (SQLException e) {
@@ -117,9 +123,32 @@ class UserCheck {
     //Admin role needed to reg
     public static void register(String role, Scanner sc) {
         if (!role.equals("admin")) {
-            System.out.println("Warning! Admin permission needed!");
-            System.out.println("You need to login as an Admin!");
-            return;
+            while(true) {
+                System.out.println(main.ANSI_RED + "Warning! Admin permission needed!" + main.ANSI_RESET);
+                System.out.println(main.ANSI_YELLOW + "You need to login as an Admin!" + main.ANSI_RESET);
+                System.out.println(main.ANSI_GREEN +"[1] Return to Login" + main.ANSI_RESET);
+                System.out.println(main.ANSI_GREEN + "[2] Return to Management Page" + main.ANSI_RESET);
+                System.out.print(main.ANSI_BLUE + "Enter your choice: " + main.ANSI_RESET);
+                int returnChoice;
+                if (sc.hasNextInt()) {
+                    returnChoice = sc.nextInt();
+                } else{
+                    System.out.println(main.ANSI_RED + "Invalid input! Please enter a number." + main.ANSI_RESET);
+                    sc.next();
+                    continue;
+                }
+                switch (returnChoice) {
+                    case 1:
+                        role = UserCheck.login(sc);
+                        break;
+                    case 2:
+                        page.managementPage(role, sc);
+                        break;
+                    default:
+                        System.out.println(main.ANSI_RED + "Invalid choice. Please try again." + main.ANSI_RESET);
+                        break;
+                }
+            }
         }
         sc.nextLine();
         System.out.print("Enter username: ");
@@ -133,7 +162,7 @@ class UserCheck {
             checkStmt.setString(1, username);
             try (ResultSet rs = checkStmt.executeQuery()) {
                 if (rs.next()) {
-                    System.out.println("Error: Username already exists!");
+                    System.out.println(main.ANSI_RED + "Error: Username already exists!" + main.ANSI_RESET);
                     return;
                 }
             }
@@ -148,13 +177,13 @@ class UserCheck {
             pstmt.setString(1, username);
             pstmt.setString(2, password);
             pstmt.executeUpdate();
-            System.out.println("Successfully registered!");
+            System.out.println(main.ANSI_PURPLE + "Successfully registered!" + main.ANSI_RESET);
         } catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println(main.ANSI_RED + "Error: " + e.getMessage() + main.ANSI_RESET);
         }
     }
 
-    public String login(Scanner sc) {
+    public static String login(Scanner sc) {
         sc.nextLine();
         System.out.print("Enter Username: ");
         String username = sc.nextLine();
@@ -172,7 +201,7 @@ class UserCheck {
             pstmtAdmin.setString(2, password);
             try (ResultSet rsAdmin = pstmtAdmin.executeQuery()) {
                 if (rsAdmin.next()) {
-                    System.out.println("Admin Login Successful!");
+                    System.out.println(main.ANSI_PURPLE + "Admin Login Successful!" + main.ANSI_RESET);
                     page.pageChoose("admin", sc);
                     return "admin";
                 }
@@ -182,24 +211,24 @@ class UserCheck {
             pstmtStaff.setString(2, password);
             try (ResultSet rsStaff = pstmtStaff.executeQuery()) {
                 if (rsStaff.next()) {
-                    System.out.println("Staff Login Successful!");
+                    System.out.println(main.ANSI_PURPLE + "Staff Login Successful!" + main.ANSI_RESET);
                     page.pageChoose("staff", sc);
                     return "staff";
                 }
             }
 
         } catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println(main.ANSI_RED + "Error: " + e.getMessage() + main.ANSI_RESET);
         }
 
-        System.out.println("Invalid credentials.");
+        System.out.println(main.ANSI_RED + "Invalid credentials." + main.ANSI_RESET);
         return "guest";
     }
 }
 
 class page {
     //After login, staff can process Borrow/Return Book here
-    //I havent add the function yet
+    //I haven't added the function yet
     //This is just a temp save
     public static void mainPage() {
 
@@ -210,29 +239,29 @@ class page {
     //So I put a Report page first lah
     public static void managementPage(String role, Scanner sc) {
         while(true) {
-            System.out.println("[1] Register Staff");
-            System.out.println("[2] Report");
-            System.out.println("[3] Exit");
-            System.out.print("Enter your choice: ");
-            String choice;
+            System.out.println(main.ANSI_GREEN + "[1] Register Staff" +main.ANSI_RESET);
+            System.out.println(main.ANSI_GREEN + "[2] Report" + main.ANSI_RESET);
+            System.out.println(main.ANSI_RED + "[0] Exit" + main.ANSI_RESET);
+            System.out.print(main.ANSI_BLUE + "Enter your choice: " + main.ANSI_RESET);
+            int choice;
             if (sc.hasNextInt()) {
-                choice = sc.nextLine();
+                choice = sc.nextInt();
             } else {
-                System.out.println("Invalid input! Please enter a number.");
+                System.out.println(main.ANSI_RED + "Invalid input! Please enter a number." + main.ANSI_RESET);
                 continue;
             }
             switch (choice) {
-                case "1":
+                case 1:
                     UserCheck.register(role, sc);
                     break;
-                case "2":
+                case 2:
                     System.out.println("Report");
                     break;
-                case "3":
+                case 0:
                     System.out.println("Exit");
                     return;
                 default:
-                    System.out.println("Invalid choice! Please try again.");
+                    System.out.println(main.ANSI_RED + "Invalid choice! Please try again." + main.ANSI_RESET);
                     break;
             }
         }
@@ -240,15 +269,16 @@ class page {
 
     public static void pageChoose(String role, Scanner sc) {
         while (true) {
-            System.out.println("1, Main Page");
-            System.out.println("2, Management Page");
-            System.out.print("Choose your choice: ");
+            System.out.println(main.ANSI_GREEN + "[1] Main Page" + main.ANSI_RESET);
+            System.out.println(main.ANSI_GREEN + "[2] Management Page" + main.ANSI_RESET);
+            System.out.println(main.ANSI_RED + "[0] Return" + main.ANSI_RESET);
+            System.out.print(main.ANSI_BLUE + "Choose your choice: " + main.ANSI_RESET);
 
             int pageChoice;
             if (sc.hasNextInt()) {
                 pageChoice = sc.nextInt();
             } else {
-                System.out.println("Invalid input! Please enter a number.");
+                System.out.println(main.ANSI_RED + "Invalid input! Please enter a number." + main.ANSI_RESET);
                 sc.next();
                 continue;
             }
@@ -261,6 +291,12 @@ class page {
                 case 2:
                     System.out.println("Management Page");
                     page.managementPage(role, sc);
+                    break;
+                case 0:
+                    System.out.println("Return");
+                    return;
+                default:
+                    System.out.println(main.ANSI_RED + "Invalid choice! Please try again." + main.ANSI_RESET);
                     break;
             }
         }
